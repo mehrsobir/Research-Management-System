01a6377d6ef1e0f12ce02e9a98fb0ccc7d7aaaf1
Mehr <mehrsobir2@gmail.com>
2023-01-19 15:17:27 +0500

    work with groups

--- users/views.py
+++ users/views.py
@@ -38,6 +38,13 @@ def user_register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
+            group_name = Group.objects.get(name='institution')
+            # user.groups.add(group_name)
+            print('fdgsd', group_name)
+            if user.is_superuser:
+                request.session['inst_role'] = True
+            if user.is_staff:
+                request.session['dep_role'] = False
             return redirect('/')
         else:
             context['registration_form'] = form
@@ -52,9 +59,16 @@ def user_login(request):
         username = request.POST['email']
         password = request.POST['password']
         user = authenticate(username = username, password = password)
-        print(request.POST)
         if user is not None:
             login(request, user)
+            # gr = Group.objects.get(name = 'institute')
+            # user.groups.add(gr)
+            if user.groups.filter(name = 'institute').exists():
+                request.session['inst_role'] = True
+                print(request.session['inst_role'], 55550)
+            if user.groups.filter(name = 'department').exists():
+                request.session['dep_role'] = True
+                print(request.session['dep_role'], 111)
             return redirect('/')
         else:
             messages.info(request, 'Логин ё парол хато аст. Лутфан, аз нав ворид шавед!!')
